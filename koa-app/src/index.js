7d343d48e716e3d9baa7bfb6575ef79224511b39
Y-lonelY <luckyyoung666@gmail.com>
2019-12-11 12:26:34 +0800

    首屏性能优化，添加gzip支持

--- koa-app/src/index.js
+++ koa-app/src/index.js
@@ -1,8 +1,8 @@
-// 引入 koa
 import Koa from 'koa';
 import Compose from 'koa-compose';
 import BodyParser from 'koa-bodyparser';
 import { listenError } from 'M/errors';
+import compress from 'koa-compress';
 import Homepage  from 'V/Homepage';
 import Practice from 'V/Practice';
 import SystemView from 'V/System';
@@ -10,8 +10,16 @@ import SystemView from 'V/System';
 // 声明一个 koa 实例
 const app = new Koa();
 
+// compress gzip 压缩
+const compressInstance = compress({
+    // 设置阈值，小于 10k 不进行压缩
+    threshold: 1024 * 10,
+    flush: require('zlib').Z_SYNC_FLUSH
+});
+
 // 合并中间件
 app.use(Compose([
+    compressInstance,
     BodyParser(), 
     Homepage, 
     Practice,
