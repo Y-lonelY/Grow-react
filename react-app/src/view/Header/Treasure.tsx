afaf2d66350f6ca995c017e34bdf68bb547ad0d8
yanGo1221 <luckyyoung666@gmail.com>
2020-05-28 21:48:50 +0800

    improvement(header): list

--- react-app/src/view/Header/Treasure.tsx
+++ react-app/src/view/Header/Treasure.tsx
@@ -4,16 +4,22 @@ import { LocaleContext } from '@/cluster/context'
 
 const { Search } = Input
 
-export default function Treasure(props) {
+function handleChange (data) {
+  console.log(data)
+}
+
+export default function Treasure() {
   // 通过 useState 来定义 state 和 setState 对，这里定义 clicked 用于标识按钮当前状态
   const [clicked, setClicked] = useState<boolean>(false)
   const { assets: { headerItems } } = useContext(LocaleContext)
-  console.log(headerItems)
+  const contenTpl = headerItems.map(item => {
+    return <div onClick={handleChange.bind(this, item)}>{item.label}</div>
+  })
   return (
     <div className="treasure-box">
       <Popover
         title=""
-        content="content"
+        content={contenTpl}
         placement="bottom"
         trigger="click"
         onVisibleChange={(visible) => {
